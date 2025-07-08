cf05b71ca37e3bcbb3de2581de170cc39d85a4d1
vaniaferreresteban <vaniaferreresteban@gmail.com>
2025-07-08 13:26:09 +0200

    feat: create sort logic in budget-list buttons

--- src/app/components/budget-list/budget-list.component.ts
+++ src/app/components/budget-list/budget-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { KeyValuePipe } from '@angular/common';
 
 import { BudgetService } from '../../services/budget.service';
+import { Client } from '../../interfaces/client';
 
 @Component({
   selector: 'app-budget-list',
@@ -12,4 +13,22 @@ import { BudgetService } from '../../services/budget.service';
 })
 export class BudgetListComponent {
   public budgetService = inject(BudgetService);
+  public selectedOrder = this.budgetService.getClientBudgets();
+  budgetsCopy = [...this.budgetService.getClientBudgets()];
+
+  public orderByAlphabet() {
+
+    const budgetsCopy = [...this.budgetService.getClientBudgets()];
+    this.selectedOrder = budgetsCopy.sort((a: Client, b: Client) => a.name.localeCompare(b.name));
+  }
+  public orderByPrice() {
+    const budgetsCopy = [...this.budgetService.getClientBudgets()];
+    this.selectedOrder = budgetsCopy.sort((a: Client, b: Client) => a.totalPrice - b.totalPrice);
+  }
+  public orderByDate() {
+    const budgetsCopy = [...this.budgetService.getClientBudgets()];
+    this.selectedOrder = budgetsCopy;
+  }
+
+
 }
